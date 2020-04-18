e09be95ef2158bf86af30182560c0cf9476c749c
MariannaRusso <37122967+MariannaRusso@users.noreply.github.com>
2020-04-18 16:13:19 +0200

    framelayouteclasseAula

--- app/src/main/java/com/example/appaulestudio/Aula.java
+++ app/src/main/java/com/example/appaulestudio/Aula.java
@@ -1,32 +1,73 @@
 package com.example.appaulestudio;
 
-public class Aula {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    private String idAula;
-    private String idUniversita;
-    private String nome;
-    private String indirizzo;
-    private double latitudine;
-    private double longitudine;
-    private boolean gruppi;
+public class Aula implements Parcelable {
 
+     String idAula;
+     String nome;
+     String luogo;
+     double latitudine;
+     double longitudine;
+     boolean gruppi;
+     int posti_liberi;
 
-    public Aula(String idAula, String idUniversita, String nome, String indirizzo, double latitudione,
-                double longitudine, boolean gruppi){
+    public Aula(String idAula, String nome, String luogo, double latitudione,
+                double longitudine, boolean gruppi, int posti_liberi){
         this.idAula=idAula;
-        this.idUniversita=idUniversita;
+
         this.nome=nome;
-        this.indirizzo=indirizzo;
+        this.luogo=luogo;
         this.latitudine=latitudione;
         this.longitudine=longitudine;
         this.gruppi=gruppi;
+        this.posti_liberi=posti_liberi;
     }
 
     public String stampaAula(Aula a){
-        String s=""+a.nome+" - "+a.idUniversita+"/n"+a.indirizzo;
+        String s=""+a.nome+" - "+"/n"+a.luogo;
         if(a.gruppi==true){
             s+="/nAula disponibile alla prenotazione per gruppi";
         }
         return s;
     }
+
+    protected Aula(Parcel in) {
+        idAula = in.readString();
+        nome = in.readString();
+        luogo = in.readString();
+        latitudine = in.readDouble();
+        longitudine = in.readDouble();
+        gruppi = in.readByte() != 0;
+        posti_liberi = in.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(idAula);
+        dest.writeString(nome);
+        dest.writeString(luogo);
+        dest.writeDouble(latitudine);
+        dest.writeDouble(longitudine);
+        dest.writeByte((byte) (gruppi ? 1 : 0));
+        dest.writeInt(posti_liberi);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Aula> CREATOR = new Creator<Aula>() {
+        @Override
+        public Aula createFromParcel(Parcel in) {
+            return new Aula(in);
+        }
+
+        @Override
+        public Aula[] newArray(int size) {
+            return new Aula[size];
+        }
+    };
 }
--- app/src/main/java/com/example/appaulestudio/Home.java
+++ app/src/main/java/com/example/appaulestudio/Home.java
@@ -5,19 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.prefs.Preferences;
+
 public class Home extends AppCompatActivity {
+    static final String URL_RICHIEDIAULE="http://pmsc9.altervista.org/progetto/richiedi_aule.php";
+    ArrayAdapter adapter;
+    String strUniversita;
+    String strNomeUniversita;
+    String strMatricola;
+    String strPassword;
+    String strStudente;
+    String strLogged;
+    SharedPreferences settings;
     protected void initUI(){
         final FrameLayout fl= findViewById(R.id.fl);
+        ListView elencoAule = findViewById(R.id.elencoAule);
+        TextView nomeAula_home= findViewById(R.id.nomeAula_home);
+        TextView luogoAula_home=findViewById(R.id.luogoAula_home);
+        TextView postiLiberi_home = findViewById(R.id.postiLiberi_home);
+
 
         //doppio frame
         final LinearLayout frameLista = (LinearLayout)findViewById(R.id.frameLista);
@@ -45,6 +76,13 @@ public class Home extends AppCompatActivity {
             }
 
         });
+         settings = getSharedPreferences("User_Preferences", Context.MODE_PRIVATE);
+         strUniversita=settings.getString("universita", null);
+         strNomeUniversita=settings.getString("nome_universita", null);
+         strMatricola=settings.getString("matricola", null);
+         strPassword=settings.getString("password", null);
+         strStudente=""+settings.getBoolean("studente", false);
+         strLogged=""+settings.getBoolean("logged", false);
     }
 
         @Override
@@ -55,25 +93,99 @@ public class Home extends AppCompatActivity {
         initUI();
         //TextView txt=findViewById(R.id.txtHome);
 
-        SharedPreferences settings = getSharedPreferences("User_Preferences", Context.MODE_PRIVATE);
-        String strUniversita=settings.getString("universita", null);
-        String strNomeUniversita=settings.getString("nome_universita", null);
-        String strMatricola=settings.getString("matricola", null);
-        String strPassword=settings.getString("password", null);
-        String strStudente=""+settings.getBoolean("studente", false);
-        String strLogged=""+settings.getBoolean("logged", false);
+
 
         Toast.makeText(getApplicationContext(),""+strNomeUniversita+" "+strMatricola+" "+strPassword+" "+strStudente+" "+strLogged,Toast.LENGTH_LONG).show();
 
     }
 
+     @Override protected void onStart(){
+        super.onStart();
 
 
-    /*@Override //creazione menu in alto
+     }
+
+    //richiedi info aule al database
+    private class listaAule extends AsyncTask<Void, Void, Aula[]> {
+        @Override
+        protected Aula[] doInBackground(Void... strings) {
+            try {
+                URL url = new URL(URL_RICHIEDIAULE);
+                //URL url = new URL("http://10.0.2.2/progetto/richiedi_aule.php");
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(1000);
+                urlConnection.setConnectTimeout(1500);
+
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+                String parametri = "codice_universita=" + URLEncoder.encode(strUniversita, "UTF-8"); //imposto parametri da passare
+                DataOutputStream dos = new DataOutputStream(urlConnection.getOutputStream());
+                dos.writeBytes(parametri);
+                dos.flush();
+                dos.close();
+
+                urlConnection.connect();
+
+                InputStream is = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+
+                String result = sb.toString();
+                JSONArray jArray = new JSONArray(result);
+                Aula[] array_aula = new Aula[jArray.length()];
+                for (int i = 0; i < jArray.length(); i++) {
+                    JSONObject json_data = jArray.getJSONObject(i);
+                    array_aula[i] = new Aula(json_data.getString("id"), json_data.getString("nome"),
+                            json_data.getString("luogo"), json_data.getDouble("latitudine"),
+                            json_data.getDouble("longitudine"), json_data.getBoolean("gruppi"),
+                            json_data.getInt("posti_liberi"));
+                }
+                return array_aula;
+            } catch (Exception e) {
+                Log.e("log_tag", "Error " + e.toString());
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Aula[] array_aula) {
+            adapter = new ArrayAdapter(Home.this,R.layout array_aula);
+            if(array_aula==null){
+                Toast.makeText().show;
+            }
+            spinner.setAdapter(adapter);
+
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    universita = (Universita) parent.getItemAtPosition(position);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+        }
+    }
+
+
+
+
+
+
+    @Override //creazione menu in alto
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.add(Menu.FIRST, 1, Menu.FIRST, "Logout");
+        menu.add(Menu.FIRST,2, Menu.FIRST+1,"Home");
         return true;
-    }*/
+    }
 
     @Override //se premo "Inserisci componente mi porta alla seconda activity"
     public boolean onOptionsItemSelected(MenuItem item) {
