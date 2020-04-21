973b69626bff7544103a84faeedfb46eb6ac9c3a
mldfr <mld.francois@gmail.com>
2020-04-21 15:59:41 +0200

    Creazione tabella sqlite

--- app/src/main/java/com/example/appaulestudio/Home.java
+++ app/src/main/java/com/example/appaulestudio/Home.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -323,4 +325,31 @@ protected void initUI(){
         finish();
         return true;
     }
+
+
+    //INIZIO PARTE LEGATA AL SALVATAGGIO DATI IN SQLITE
+    private final static int DB_VERSION=1; //La costante mi serve per gli aggiornamenti
+    //CREO LA CLASSE DBHelper che nasce con due metodi in ovveride che vengono chiamati quando
+    //il database deve essere creato o aggiornato
+    private final SQLiteOpenHelper dbHelper = new SQLiteOpenHelper(this, "infobase_aule", null, DB_VERSION) {
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            String sql = "CREATE TABLE \"infobase_aule\" (\n" +
+                    "\t\"id\"\tTEXT NOT NULL,\n" +
+                    "\t\"luogo\"\tTEXT NOT NULL,\n" +
+                    "\t\"posti_liberi\"\tINTEGER NOT NULL,\n" +
+                    "\t\"gruppi\"\tINTEGER NOT NULL,\n" +
+                    "\tPRIMARY KEY(\"id\")\n" +
+                    ")";
+            db.execSQL(sql);
+
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        }
+    };
 }
